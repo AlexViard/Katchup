9382896e948b454f99bc70c0798fa35778e21a81
Pol-Erwan <polerwankite@gmail.com>
2019-11-25 15:22:36 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'index#items'
   devise_for :users
-  ressources: :carts
-  ressources: :items
-  ressources: :orders
+  resources :carts
+  resources :items
+  resources :orders
 end
