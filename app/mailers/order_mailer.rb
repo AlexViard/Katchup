3dd43625c739ffde45aa6ecc3a5291c808830cdd
evapapazian <eva.papazian@ieseg.fr>
2019-11-26 18:19:41 +0100

    mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -11,6 +11,6 @@ class OrderMailer < ApplicationMailer
   	def administrator_email(order)
   		@order = order
   		@url = 'http://katchup-app.heroku.app.com' 
-		mail(to: thp.mtp.best.groupe@gmail.com, subject:'Une nouvelle commande a été passée') #Don't forget to modify the email (use the administrator's)
+		  mail(to: thp.mtp.best.groupe@gmail.com, subject:'Une nouvelle commande a été passée') #Don't forget to modify the email (use the administrator's)
   	end
 end
