96dddbab7421bca6bc13e198be3dd92214c48341
Pol-Erwan <polerwankite@gmail.com>
2019-11-26 16:02:00 +0100

    update profil avatar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :users do
-  	resources :charges, only: [:create]
+  	resources :avatar, only: [:create]
   end
   resources :carts
   resources :items
