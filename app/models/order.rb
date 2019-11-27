edd7494bfc554eb7487cc5b78b893a992875f35a
AlienFR <alex-viard@hotmail.fr>
2019-11-27 19:00:06 +0100

    panel_user_create

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
 	has_many :items
 	belongs_to :user
 
-<<<<<<< HEAD
+
 	after_create :customer_email
 	after_create :administrator_email
 
@@ -13,16 +13,4 @@ class Order < ApplicationRecord
   	def administrator_email
   	  OrderMailer.administrator_email(self, self.user).deliver_now
   	end
-=======
-	#after_create :customer_email
-	#after_create :administrator_email
-
-	#def customer_email
-  #	  OrderMailer.customer_email(self).deliver_now
-  #	end
-
-  #	def admnistrator_email
-  #	  OrderMailer.administrator_email(self).deliver_now
-  #	end 
->>>>>>> a21a58bf55d71359ceebd894300b49421aa80050
 end
