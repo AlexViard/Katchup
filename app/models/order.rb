3c80f3895131dc3349344b8db81336251f537de7
Lea-Brml <l.bramoulle@hotmail.fr>
2019-11-27 18:00:26 +0100

    my profile finish

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,6 @@ class Order < ApplicationRecord
 	has_many :items
 	belongs_to :user
 
-<<<<<<< HEAD
 	after_create :customer_email
 	after_create :administrator_email
 
@@ -13,7 +12,6 @@ class Order < ApplicationRecord
   	def administrator_email
   	  OrderMailer.administrator_email(self, self.user).deliver_now
   	end
-=======
 	#after_create :customer_email
 	#after_create :administrator_email
 
@@ -23,6 +21,5 @@ class Order < ApplicationRecord
 
   #	def admnistrator_email
   #	  OrderMailer.administrator_email(self).deliver_now
-  #	end 
->>>>>>> a21a58bf55d71359ceebd894300b49421aa80050
+  #	end
 end
