b76cc19971418c68384ebfac4312c3fcc69224b4
AlienFR <alex-viard@hotmail.fr>
2019-11-27 16:18:10 +0100

    admin_mailer

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,14 @@ class Order < ApplicationRecord
 	has_many :items
 	belongs_to :user
 
-	after_create :customer_email 
+	after_create :customer_email
 	after_create :administrator_email
 
 	def customer_email
-  	  OrderMailer.customer_email(self).deliver_now
+  	  OrderMailer.customer_email(self, self.user).deliver_now
   	end
 
-  	def admnistrator_email
-  	  OrderMailer.administrator_email(self).deliver_now
-  	end 
+  	def administrator_email
+  	  OrderMailer.administrator_email(self, self.user).deliver_now
+  	end
 end
