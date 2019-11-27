6a3458bb8ad4a4e3c324c7f4f05a22cf72890fb5
AlienFR <alex-viard@hotmail.fr>
2019-11-27 19:24:36 +0100

    conflict

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,6 @@ class Order < ApplicationRecord
 	has_many :items
 	belongs_to :user
 
-<<<<<<< HEAD
-
-=======
->>>>>>> caecc6b8746d46132ff479aecb724efa2e2f8188
 	after_create :customer_email
 	after_create :administrator_email
 
@@ -16,18 +12,5 @@ class Order < ApplicationRecord
   	def administrator_email
   	  OrderMailer.administrator_email(self, self.user).deliver_now
   	end
-<<<<<<< HEAD
-=======
-	#after_create :customer_email
-	#after_create :administrator_email
 
-	#def customer_email
-  #	  OrderMailer.customer_email(self).deliver_now
-  #	end
-
-  #	def admnistrator_email
-  #	  OrderMailer.administrator_email(self).deliver_now
-  #	end
-  #	end
->>>>>>> caecc6b8746d46132ff479aecb724efa2e2f8188
 end
